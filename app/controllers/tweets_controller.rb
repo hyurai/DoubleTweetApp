a3dfb2f04277f78856551db4373020d8c08bd310
hyurai <yuumanagai0504@icloud.com>
2021-08-29 07:52:30 +0000

    common_tweet_comp

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,7 @@ class TweetsController < ApplicationController
     end
     def create
         tweet = Tweet.create(admin_id: params[:admin_id],user_id: params[:user_id],text: params[:text])
+        tweet.save
         redirect_to '/tweets'
     end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,4 @@
 class Tweet < ApplicationRecord
-    belongs_to :user
-    belongs_to :admin
+    belongs_to :user,optional: true
+    belongs_to :admin,optional: true
 end
